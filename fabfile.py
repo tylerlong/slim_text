e922ee54099bf6ccbd9ce516fef7d08ba4fdf780
Tyler Long <tyler4long@gmail.com>
2013-05-04 02:24:14 +0800

    Upgrade fabric script to use the latest fabric api

--- fabfile.py
+++ fabfile.py
@@ -1,20 +1,14 @@
-from fabric.api import *
-from contextlib import contextmanager as _contextmanager
+from fabric.api import task, cd, local
 
-env.user = 'tyler'
-env.directory = '/home/tyler/src/win/javascript/slim_text/'
-env.hosts = ['localhost']
+PROJECT_DIR = '/home/tyler/src/win/javascript/slim_text/'
 
-@_contextmanager
-def workspace():
-    with cd(env.directory):
-        yield
-
+@task
 def coffee():
     """start coffeescript daemon to compile dynamically"""
-    with workspace():
-        run('coffee --watch --output js/ --compile coffee/')
+    with cd(PROJECT_DIR):
+        local('coffee --watch --output js/ --compile coffee/')
 
+@task
 def dist():
     """generate distribution package inside the dist/ folder"""
     import json
@@ -22,43 +16,43 @@ def dist():
     with open('manifest.json', 'r') as file:
         data = json.loads(file.read())
         folder = '../slimtext.org/dist/{0}'.format(data['version'])
-    with workspace():
-        run('mkdir -p {0}/'.format(folder))
-        run('rm -rf {0}/*'.format(folder))
+    with cd(PROJECT_DIR):
+        local('mkdir -p {0}/'.format(folder))
+        local('rm -rf {0}/*'.format(folder))
         
-        run('cp -r _locales {0}/'.format(folder))
+        local('cp -r _locales {0}/'.format(folder))
         
-        run('mkdir -p {0}/ace/src-min-noconflict/'.format(folder))
-        run('cp -r ace/src-min-noconflict/* {0}/ace/src-min-noconflict/'.format(folder))
+        local('mkdir -p {0}/ace/src-min-noconflict/'.format(folder))
+        local('cp -r ace/src-min-noconflict/* {0}/ace/src-min-noconflict/'.format(folder))
         
-        run('mkdir -p {0}/css/'.format(folder))
-        run('scss -t compressed css/bootstrap.css {0}/css/bootstrap.css'.format(folder))
-        run('scss -t compressed css/editor.css {0}/css/editor.css'.format(folder))
-        run('scss -t compressed css/jquery.layout.css {0}/css/jquery.layout.css'.format(folder))
-        run('scss -t compressed css/jqueryui.css {0}/css/jqueryui.css'.format(folder))
-        run('scss -t compressed css/options.css {0}/css/options.css'.format(folder))
-        run('cp -r -n css/* {0}/css/'.format(folder))
+        local('mkdir -p {0}/css/'.format(folder))
+        local('scss -t compressed css/bootstrap.css {0}/css/bootstrap.css'.format(folder))
+        local('scss -t compressed css/editor.css {0}/css/editor.css'.format(folder))
+        local('scss -t compressed css/jquery.layout.css {0}/css/jquery.layout.css'.format(folder))
+        local('scss -t compressed css/jqueryui.css {0}/css/jqueryui.css'.format(folder))
+        local('scss -t compressed css/options.css {0}/css/options.css'.format(folder))
+        local('cp -r -n css/* {0}/css/'.format(folder))
         
-        run('mkdir -p {0}/font/'.format(folder))
-        run('scss -t compressed font/fonts.css {0}/font/fonts.css'.format(folder))
-        run('cp -r -n font/* {0}/font/'.format(folder))
+        local('mkdir -p {0}/font/'.format(folder))
+        local('scss -t compressed font/fonts.css {0}/font/fonts.css'.format(folder))
+        local('cp -r -n font/* {0}/font/'.format(folder))
         
-        run('cp -r html/ {0}/'.format(folder))
+        local('cp -r html/ {0}/'.format(folder))
         
-        run('mkdir -p {0}/image/'.format(folder))  
-        run('cp -r image/* {0}/image/'.format(folder))
+        local('mkdir -p {0}/image/'.format(folder))  
+        local('cp -r image/* {0}/image/'.format(folder))
         
-        run('mkdir -p {0}/js/'.format(folder))        
-        run('uglifyjs js/application.js -c -m -o {0}/js/application.js'.format(folder))
-        run('uglifyjs js/background.js -c -m -o {0}/js/background.js'.format(folder))
-        run('uglifyjs js/editor.js -c -m -o {0}/js/editor.js'.format(folder))
-        run('uglifyjs js/event.js -c -m -o {0}/js/event.js'.format(folder))
-        run('uglifyjs js/file_manager.js -c -m -o {0}/js/file_manager.js'.format(folder))
-        run('uglifyjs js/main.js -c -m -o {0}/js/main.js'.format(folder))
-        run('uglifyjs js/mode.js -c -m -o {0}/js/mode.js'.format(folder))
-        run('uglifyjs js/option.js -c -m -o {0}/js/option.js'.format(folder))
-        run('uglifyjs js/util.js -c -m -o {0}/js/util.js'.format(folder))
-        run('cp -r -n js/* {0}/js/'.format(folder))
+        local('mkdir -p {0}/js/'.format(folder))        
+        local('uglifyjs js/application.js -c -m -o {0}/js/application.js'.format(folder))
+        local('uglifyjs js/background.js -c -m -o {0}/js/background.js'.format(folder))
+        local('uglifyjs js/editor.js -c -m -o {0}/js/editor.js'.format(folder))
+        local('uglifyjs js/event.js -c -m -o {0}/js/event.js'.format(folder))
+        local('uglifyjs js/file_manager.js -c -m -o {0}/js/file_manager.js'.format(folder))
+        local('uglifyjs js/main.js -c -m -o {0}/js/main.js'.format(folder))
+        local('uglifyjs js/mode.js -c -m -o {0}/js/mode.js'.format(folder))
+        local('uglifyjs js/option.js -c -m -o {0}/js/option.js'.format(folder))
+        local('uglifyjs js/util.js -c -m -o {0}/js/util.js'.format(folder))
+        local('cp -r -n js/* {0}/js/'.format(folder))
         
-        run('cp manifest.json {0}/'.format(folder))
-        run('cp -r plugin/ {0}/'.format(folder))
+        local('cp manifest.json {0}/'.format(folder))
+        local('cp -r plugin/ {0}/'.format(folder))
